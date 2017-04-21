656776bd82cac907632de6ecd84e5aa3c8897305
“Vilde <vilde@vevatne.no>
2017-04-21 16:08:09 -0700

    Remove white lines

--- lib/help_me_p.rb
+++ lib/help_me_p.rb
@@ -32,12 +32,10 @@ module HelpMeP
 
   def pee(desired_print, method = __method__.to_s)
     puts HelpMeP.colorize("*" * 50, HelpMeP.blue)
-    puts "*" * 50
     puts HelpMeP.colorize(" " * 23 + "START" + " " * 22, HelpMeP.blue)
-    puts "*" * 50
     puts HelpMeP.colorize("*" * 50, HelpMeP.blue)
     if !desired_print
-      puts HelpMeP.colorize("😡 😡 😡 RETURNED NIL VALUE 😡 😡 😡", HelpMeP.red)
+      puts HelpMeP.colorize("😡   😡   😡   RETURNED NIL VALUE   😡   😡   😡", HelpMeP.red)
     else
       puts HelpMeP.colorize("PRINTING OBJECT OF TYPE: #{desired_print.class.to_s}", HelpMeP.green)
       puts "_" * 50
@@ -45,9 +43,7 @@ module HelpMeP
       puts "_" * 50
     end
     puts HelpMeP.colorize("*" * 50, HelpMeP.blue)
-    puts "*" * 50
     puts HelpMeP.colorize(" " * 23 + "END" + " " * 24, HelpMeP.blue)
-    puts "*" * 50
     puts HelpMeP.colorize("*" * 50, HelpMeP.blue)
   end
 end
